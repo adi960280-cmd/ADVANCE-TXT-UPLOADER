897a06cdba2410a0bbdda85d1c5f142fe10886a8
adi960280-cmd <adityamalhotra960@gmail.com>
2026-01-01 13:57:37 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,5 +1,9 @@
 # Don't Remove Credit Tg - @Tushar0125
 # Ask Doubt on telegram @Tushar0125
-
 from os import environ
+
+API_ID = int(environ.get("API_ID", "29115102")) #Replace with your api id
+API_HASH = environ.get("API_HASH", "1a331db2b00e9d2decaa9c7276449eb6") #Replace with your api hash
+BOT_TOKEN = environ.get("BOT_TOKEN", "7903742155:AAGLWumrUPxZpl0yakb4oDhfuq74wtWRzdY") #Replace with your bot token
+
 
